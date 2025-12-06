1a7e61b48fad5b39c74d0443185289a6765eac5f
Chaim Lev-Ari <chiptus@gmail.com>
2025-12-06 19:40:42 +0200

    init

---
+++ wxt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-react"],
+  browser: "chromium",
+  webExt: {
+    binaries: {
+      chromium: "/Applications/Chromium.app/Contents/MacOS/Chromium",
+    },
+  },
+});
